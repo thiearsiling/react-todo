a48e85b0d8abb4aa677ec8c820227e5894918909
thiago.oliveira <thiago.aa.oliveira@gmail.com>
2020-07-02 21:02:43 -0300

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App container">Hello World</div>;
+}
+
+export default App;
